9c8902de8858feda079060b7222f558b1bf77ae8
Saegl <saegl@protonmail.com>
2025-03-27 19:27:38 +0500

    Better binop codegen

--- noname.py
+++ noname.py
@@ -10,31 +10,47 @@ import parser
 
 
 class Codegen:
-    def __init__(self):
+    var_offsets: dict[str, int]
+    last_offset: int
+
+    def __init__(self) -> None:
         self.var_offsets = {}
         self.last_offset = -4
 
-    def codegen_expr(self, output: TextIOWrapper, expr: parser.Expr):
+    def codegen_expr(self, output: TextIOWrapper, expr: parser.Expr) -> str:
         match expr:
             case parser.IntLiteral(i):
-                output.write(f"\tmovl\t${i}, %eax\n")
+                output.write(f"\tmovl\t${i}, %eax\t# load int literal\n")
                 return "%eax"
+
             case parser.Var(name):
-                return f"{self.var_offsets[name]}(%rbp)"
-            case parser.BinOp(parser.IntLiteral(lhs), op, parser.IntLiteral(rhs)):
+                offset = self.var_offsets[name]
+                output.write(f"\tmovl\t{offset}(%rbp), %eax\t# load var '{name}'\n")
+                return "%eax"
+
+            case parser.BinOp(lhs, op, rhs):
                 ops = {
                     "+": "addl",
                     "-": "subl",
                     "*": "imull",
                 }
                 op_cmd = ops[op]
-                output.write(f"\tmovl\t${lhs}, %eax\n")
-                output.write(f"\t{op_cmd}\t${rhs}, %eax\n")
+
+                self.codegen_expr(output, lhs)
+                output.write(f"\tpushq\t%rax\t\t# save lhs for {op}\n")
+
+                self.codegen_expr(output, rhs)
+
+                output.write("\tmovl\t%eax, %ecx\t# move rhs to 'ecx'\n")
+                output.write(f"\tpopq\t%rax\t\t# restore lhs for {op}\n")
+                output.write(f"\t{op_cmd}\t%ecx, %eax\t# do {op}\n")
+
                 return "%eax"
+
             case other:
                 raise Exception(f"Unexpected Expr {other}")
 
-    def codegen(self, filename: str, fn: parser.Function):
+    def codegen(self, filename: str, fn: parser.Function) -> None:
         output = open("program.s", "w")
 
         output.write(f'\t.file\t"{filename}"\n')
@@ -44,22 +60,27 @@ class Codegen:
 
         output.write(f"{fn.name}:\n")  # label where function starts
         # save %rbp value in stack, to restore it in the end
+        output.write("\t# save stack\n")
         output.write("\tpushq\t%rbp\n")
         # establish new stack frame for `main`
         output.write("\tmovq\t%rsp, %rbp\n")
+        output.write("\n")
 
         for statement in fn.block.statements:
             match statement:
                 case parser.Return(expr):
                     value = self.codegen_expr(output, expr)
-                    output.write(f"\tmovl\t{value}, %eax\n")
+                    output.write(f"\tmovl\t{value}, %eax\t# return\n")
                 case parser.DeclVariable(name, parser.IntLiteral(i)):
                     self.var_offsets[name] = self.last_offset
                     self.last_offset -= 4
-                    output.write(f"\tmovl\t${i}, {self.var_offsets[name]}(%rbp)\n")
+                    output.write(
+                        f"\tmovl\t${i}, {self.var_offsets[name]}(%rbp)\t# decl '{name}'\n"
+                    )
                 case other:
                     raise Exception(f"Unexpected stmt {other}")
 
+        output.write("\n\t# restore stack\n")
         output.write("\tpopq\t%rbp\n")  # restore previous base pointer from stack
         output.write("\tret\n")  # return control to the caller
 
@@ -73,7 +94,7 @@ class Codegen:
         output.close()
 
 
-def main():
+def main() -> None:
     argparser = argparse.ArgumentParser()
     argparser.add_argument("file", type=pathlib.Path)
     argparser.add_argument("--ast", action="store_true")
@@ -97,9 +118,10 @@ def main():
 
     if args.run:
         print("\n===RUN===")
-        subprocess.run(["gcc", "program.s"])
-        out = subprocess.run(["./a.out"])
-        print(out.returncode)
+        compilation_result = subprocess.run(["gcc", "program.s"])
+        if compilation_result.returncode == 0:
+            out = subprocess.run(["./a.out"])
+            print(out.returncode)
 
 
 if __name__ == "__main__":
